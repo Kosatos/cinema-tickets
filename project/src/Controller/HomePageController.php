00582cbe683a5183075800458160bb3d2e00bfc4
Вячеслав Макаров <makarov@positron-it.ru>
2023-08-22 08:53:10 +0300

    add filter for main session to home page

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use DateTimeImmutable;
 use App\Repository\CinemaRepository;
+use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(CinemaRepository $cinemaRepository): Response
-    {
-        $films = $cinemaRepository->findAll();
+	#[Route('/', name: 'homepage')]
+	public function index(SessionRepository $sessionRepository): Response
+	{
+		$data = new DateTimeImmutable();
+		$sessionData = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', "{$data->format('Y-m-d')} 00:01:00");
 
-        return $this->render('pages/home.html.twig', compact('films'));
-    }
+		$mainSessions = $sessionRepository->findBy(['data' => $sessionData]);
+
+		return $this->render('pages/home.html.twig', compact('mainSessions'));
+	}
 }
