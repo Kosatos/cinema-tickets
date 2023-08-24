46f661941748d93041846773dde261c6d85065bc
kosatos <kosatosfront@yandex.ru>
2023-08-24 09:41:00 +0300

    resolve films on home page

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -19,16 +19,23 @@ class HomePageController extends AbstractController
 	{
 		$data = (new DateTimeImmutable())->format('Y-m-d');
 
-		$currentDataFilms = array_filter($cinemaRepository->findAll(), fn(Cinema $cinema) => $cinema->getSessions()
-				->filter(fn(Session $session) => $session->getData()->format('Y-m-d') === $data)->count() > 0
+		$currentDataFilms = array_filter(
+			$cinemaRepository->findAll(),
+			fn(Cinema $cinema) => $cinema->getSessions()->filter(fn(Session $session) => $session->getData()->format('Y-m-d') === $data)->count() > 0
+		);
+
+		$currentDataSessionHalls = array_filter(
+			$hallRepository->findAll(),
+			fn(Hall $hall) => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data)
 		);
 
 		$films = array_map(fn(Cinema $cinema) => [
 			'cinema' => $cinema,
 			'halls' => array_map(fn(Hall $hall) => [
 				'hall' => $hall->getNumber(),
-				'sessions' => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data),
-			], $hallRepository->findAll())
+				'sessions' => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data)
+			],
+				$currentDataSessionHalls),
 		], $currentDataFilms);
 
 		return $this->render('pages/home.html.twig', compact('films'));
