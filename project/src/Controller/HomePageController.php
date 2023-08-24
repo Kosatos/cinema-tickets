bc4b0e0fc41b21dc91ea27e0afab7e05232ed951
kosatos <kosatosfront@yandex.ru>
2023-08-24 21:24:47 +0300

    add filter data to homepage

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -8,37 +8,46 @@ use DateTimeImmutable;
 use App\Entity\Session;
 use App\Repository\HallRepository;
 use App\Repository\CinemaRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
-	#TODO перенести логику поиска залов для фильмов, и сессий для залов в репозитории... потренироваться нужно будет
-	#[Route('/', name: 'homepage')]
-	public function index(CinemaRepository $cinemaRepository, HallRepository $hallRepository): Response
-	{
-		$data = (new DateTimeImmutable())->format('Y-m-d');
+    #TODO перенести логику поиска залов для фильмов, и сессий для залов в репозитории... потренироваться нужно будет
+    /**
+     * @throws Exception
+     */
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request, CinemaRepository $cinemaRepository, HallRepository $hallRepository): Response
+    {
+        if ($request->query->has('films')) {
+            $data = $request->query->get('films');
+        } else {
+            $data = (new DateTimeImmutable())->format('Y-m-d');
+        }
 
-		$currentDataFilms = array_filter(
-			$cinemaRepository->findAll(),
-			fn(Cinema $cinema) => $cinema->getSessions()->filter(fn(Session $session) => $session->getData()->format('Y-m-d') === $data)->count() > 0
-		);
+        $currentDataFilms = array_filter(
+            $cinemaRepository->findAll(),
+            fn(Cinema $cinema) => $cinema->getSessions()->filter(fn(Session $session) => $session->getData()->format('Y-m-d') === $data)->count() > 0
+        );
 
-		$currentDataSessionHalls = array_filter(
-			$hallRepository->findAll(),
-			fn(Hall $hall) => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data)
-		);
+        $currentDataSessionHalls = array_filter(
+            $hallRepository->findAll(),
+            fn(Hall $hall) => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data)
+        );
 
-		$films = array_map(fn(Cinema $cinema) => [
-			'cinema' => $cinema,
-			'halls' => array_map(fn(Hall $hall) => [
-				'hall' => $hall->getNumber(),
-				'sessions' => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data)
-			],
-				$currentDataSessionHalls),
-		], $currentDataFilms);
+        $films = array_map(fn(Cinema $cinema) => [
+            'cinema' => $cinema,
+            'halls' => array_map(fn(Hall $hall) => [
+                'hall' => $hall->getNumber(),
+                'sessions' => array_filter($hall->getSessions()->toArray(), fn(Session $session) => $session->getHall()->getNumber() == $hall->getNumber() && $session->getData()->format('Y-m-d') == $data)
+            ],
+                $currentDataSessionHalls),
+        ], $currentDataFilms);
 
-		return $this->render('pages/home.html.twig', compact('films'));
-	}
+        return $this->render('pages/home.html.twig', compact('films'));
+    }
 }
