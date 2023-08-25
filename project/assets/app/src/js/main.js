adc0ab9fa0f1950c1161b5d7c36e9ae70397a474
kosatos <kosatosfront@yandex.ru>
2023-08-25 18:01:10 +0300

    add films request

--- project/assets/app/src/js/main.js
+++ project/assets/app/src/js/main.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', (evt) => {
         const chair = evt.target.closest('.buying-scheme__chair')
@@ -7,6 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 chair.classList.remove('buying-scheme__chair_selected')
             }
+        }
+
+        const day = evt.target.closest('.page-nav__day')
+        if (day && !day.classList.contains('page-nav__day_chosen')) {
+            document.querySelector('.page-nav__day_chosen').classList.remove('page-nav__day_chosen')
+            day.classList.add('page-nav__day_chosen')
+            axios(`?films=${day.dataset.date}`, {
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            }).then(response => {
+                const wrapper = document.querySelector('.sessions-wrapper')
+                wrapper.innerHTML = response.data
+            }).catch(e => console.log(e))
         }
     })
 })
