059a26cfacbcbae1dab9c355210fb96c9de83440
novapc74 <novapc@ukr.net>
2023-09-02 10:09:08 +0300

    remove comment in cinemaController

--- project/src/Controller/CinemaController.php
+++ project/src/Controller/CinemaController.php
@@ -16,17 +16,6 @@ class CinemaController extends AbstractController
     #[Route('/session/{slug}', name: 'app_show_cinema')]
     public function showCinema(?Session $session): Response|NotFoundHttpException
     {
-//        $seats = array_map(fn(Seat $seat) => [
-//            'seatId' => $seat->getId(),
-//            'identifier' => [
-//                'row' => $seat->getIdentifier()[0],
-//                'place' => $seat->getIdentifier()[1],
-//            ],
-//            'isVip' => $seat->isIsVip(),
-//            'hasTicket' => $seat->hasTicket($session, $seat),
-//        ], $session->getHall()->getSeats()->toArray());
-
-
         $seats = $session->getHall()->getSeats();
 
         return $this->render('pages/session.html.twig', compact('session', 'seats'));
