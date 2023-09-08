6f6a6d318595bd61c43d95f9d7ada73a81b655ad
kosatos <kosatosfront@yandex.ru>
2023-09-08 16:04:14 +0300

    add violation for not unique seat

--- project/src/Controller/Admin/SeatCrudController.php
+++ project/src/Controller/Admin/SeatCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Seat;
+use App\Repository\SeatRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -13,34 +16,63 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 
 class SeatCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return Seat::class;
-    }
+	public function __construct(private readonly SeatRepository $seatRepository)
+	{
+	}
 
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud
-            ->setEntityLabelInPlural('Места')
-            ->setEntityLabelInSingular('Место');
-    }
+	public static function getEntityFqcn(): string
+	{
+		return Seat::class;
+	}
 
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            ArrayField::new('identifier', 'Ряд и Место')
-                ->setTextAlign('center')
-                ->setColumns('col-sm-6 col-lg-5 col-xxl-3')
-                ->setHelp('Идентифицируйте место в формате: <p><strong style="color: #7c2d12">НОМЕР РЯДА, НОМЕР МЕСТА</strong></p>')
-            ,
-            BooleanField::new('isVip', 'VIP')
-                ->onlyOnIndex()
-            ,
-            FormField::addRow(),
-            AssociationField::new('hall', 'Зал')
-                ->setTextAlign('center')
-                ->setColumns('col-sm-6 col-lg-5 col-xxl-3')
-                ->setHelp('Выберете номер зала, для которого формируется схема мест.')
-        ];
-    }
+	public function configureCrud(Crud $crud): Crud
+	{
+		return $crud
+			->setEntityLabelInPlural('Места')
+			->setEntityLabelInSingular('Место');
+	}
+
+	public function configureFields(string $pageName): iterable
+	{
+		return [
+			ArrayField::new('identifier', 'Ряд и Место')
+				->setTextAlign('center')
+				->setColumns('col-sm-6 col-lg-5 col-xxl-3')
+				->setHelp('Идентифицируйте место в формате: <p><strong style="color: #7c2d12">НОМЕР РЯДА, НОМЕР МЕСТА</strong></p>')
+				->setFormTypeOptions([
+					'constraints' => [
+						new Callback([
+							$this,
+							'validate'
+						])
+					],
+					'error_bubbling' => false,
+				])
+			,
+			BooleanField::new('isVip', 'VIP')
+				->onlyOnIndex()
+			,
+			FormField::addRow(),
+			AssociationField::new('hall', 'Зал')
+				->setTextAlign('center')
+				->setColumns('col-sm-6 col-lg-5 col-xxl-3')
+				->setHelp('Выберете номер зала, для которого формируется схема мест.')
+		];
+	}
+
+	public function validate($identifier, ExecutionContextInterface $context): void
+	{
+		/**@var Seat $currentSeat */
+		$currentSeat = $this->getContext()->getEntity()->getInstance();
+
+		$identifier = array_values($identifier);
+
+		$invalidSeat = array_filter($this->seatRepository->findAll(), fn(Seat $seat) => $currentSeat !== $seat && $seat->getIdentifier()[0] == $identifier[0] && $seat->getIdentifier()[1] == $identifier[1] && $seat->getHall() === $currentSeat->getHall());
+
+		if (count($invalidSeat) > 0) {
+			$context->buildViolation('Такое место в этом кинозале уже существует')
+				->atPath('identifier')
+				->addViolation();
+		}
+	}
 }
--- project/src/Entity/Seat.php
+++ project/src/Entity/Seat.php
@@ -8,102 +8,111 @@ use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SeatRepository::class)]
+#[ORM\HasLifecycleCallbacks()]
 class Seat
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private ?int $id = null;
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column(type: 'integer')]
+	private ?int $id = null;
 
-    #[Assert\Count(
-        min: 2,
-        max: 2,
-        minMessage: 'Вторым параметром укажите место в ряду',
-        maxMessage: 'Первый элемент - ряд, второй - место. Остальные параметры избыточны.'
-    )]
-    #[ORM\Column(type: 'json')]
-    private array $identifier = [];
+	#[Assert\Count(
+		min: 2,
+		max: 2,
+		minMessage: 'Вторым параметром укажите место в ряду',
+		maxMessage: 'Первый элемент - ряд, второй - место. Остальные параметры избыточны.'
+	)]
+	#[ORM\Column(type: 'json')]
+	private array $identifier = [];
 
-    #[ORM\Column(type: 'boolean')]
-    private bool $isVip;
+	#[ORM\Column(type: 'boolean')]
+	private bool $isVip;
 
-    #[ORM\ManyToOne(targetEntity: Hall::class, cascade: ['persist'], inversedBy: 'seats')]
-    private ?Hall $hall;
+	#[ORM\ManyToOne(targetEntity: Hall::class, cascade: ['persist'], inversedBy: 'seats')]
+	private ?Hall $hall;
 
-    #[Assert\Callback]
-    public function validate(ExecutionContextInterface $context): void
-    {
-        $seatsForCurrentHall = array_map(fn(Seat $seat) => $this->getIdentifier(), $this->getHall()->getSeats()->toArray());
+	#[Assert\Callback]
+	public function validate(ExecutionContextInterface $context): void
+	{
+		$seatsForCurrentHall = array_map(fn(Seat $seat) => $this->getIdentifier(), $this->getHall()->getSeats()->toArray());
 
-        $object = array_filter($this->getHall()->getSeats()->toArray(), fn(Seat $seat) => $this != $seat && in_array($seat->getIdentifier(), $seatsForCurrentHall));
+		$object = array_filter($this->getHall()->getSeats()->toArray(), fn(Seat $seat) => $this != $seat && in_array($seat->getIdentifier(), $seatsForCurrentHall));
 
-        if (count($object) > 0) {
-            $context->buildViolation('Место с такими параметрами уже существует.')
-                ->atPath('identifier')
-                ->addViolation();
-        }
-    }
+		if (count($object) > 0) {
+			$context->buildViolation('Место с такими параметрами уже существует.')
+				->atPath('identifier')
+				->addViolation();
+		}
+	}
 
-    public function __toString(): string
-    {
-        $seat = $this->identifier;
+	public function __toString(): string
+	{
+		$seat = $this->identifier;
 
-        return "Ряд - {$seat[0]}, место - {$seat[1]}.";
-    }
+		return "Ряд - {$seat[0]}, место - {$seat[1]}.";
+	}
 
-    public function __construct()
-    {
-        $this->isVip = false;
-    }
+	public function __construct()
+	{
+		$this->isVip = false;
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getIdentifier(): ?array
-    {
-        return $this->identifier;
-    }
+	public function getIdentifier(): ?array
+	{
+		return $this->identifier;
+	}
 
-    public function setIdentifier(array $identifier): self
-    {
-        $this->identifier = $identifier;
+	public function setIdentifier(array $identifier): self
+	{
+		$this->identifier = $identifier;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function isIsVip(): ?bool
-    {
-        return $this->isVip;
-    }
+	public function isIsVip(): ?bool
+	{
+		return $this->isVip;
+	}
 
-    public function setIsVip(?bool $isVip): self
-    {
-        $this->isVip = $isVip;
+	public function setIsVip(?bool $isVip): self
+	{
+		$this->isVip = $isVip;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function isActive(): bool
-    {
-        return false;
-    }
+	public function isActive(): bool
+	{
+		return false;
+	}
 
-    public function getHall(): ?Hall
-    {
-        return $this->hall;
-    }
+	public function getHall(): ?Hall
+	{
+		return $this->hall;
+	}
 
-    public function setHall(?Hall $hall): self
-    {
-        $this->hall = $hall;
+	public function setHall(?Hall $hall): self
+	{
+		$this->hall = $hall;
 
-        return $this;
-    }
+		return $this;
+	}
 
-     public function hasTicket(Session $session): bool
-     {
-         return (bool)$session->getTickets()->filter(fn(Ticket $ticket) => $ticket->getSeat() === $this)->current();
-     }
+	public function hasTicket(Session $session): bool
+	{
+		return (bool)$session->getTickets()->filter(fn(Ticket $ticket) => $ticket->getSeat() === $this)->current();
+	}
+
+	#[ORM\PrePersist]
+	#[ORM\PreUpdate]
+	public function prePersist(): void
+	{
+		$data = array_values($this->getIdentifier());
+		$this->identifier=  array_map(fn($item) => (int) $item, $data);
+	}
 }
